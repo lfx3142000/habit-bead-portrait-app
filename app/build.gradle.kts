377b2400c5710e5de88cb31d48a654a96705df62
lfx3142000 <lfx3142000@gmail.com>
2026-06-10 00:58:52 -0400

    Align Android JVM target with Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,11 @@ android {
         versionCode = 1
         versionName = "0.1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
 
 dependencies {
